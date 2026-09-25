e7fa1cdc7c537aac76c84cca78d0ab4a928f0391
Sonam Tshering <sonamtshering1213@gmail.com>
2026-09-25 10:47:52 -0400

    Edited code logic

--- deliverycalculator.py
+++ deliverycalculator.py
@@ -4,16 +4,17 @@ if order_total.replace(".",'',1).isdigit():
     if order_total>0 and order_total<=1000:
         delivery_day=input("Enter the delivery day (e.g. Monday): ")
         delivery_day=delivery_day.lower()
-        week_days=["monday","tuesday","wednesday","thursday","friday","saturday","sunday"]
-        weekend=["saturday","sunday"]
-        if delivery_day in week_days:
-            if delivery_day in weekend and order_total<50:
+        week_days=(delivery_day=="monday" or delivery_day=="tuesday" or delivery_day=="wednesday" or delivery_day=="thursday" or
+                   delivery_day=="friday" or delivery_day=="10saturday" or delivery_day=="sunday") 
+        weekend=(delivery_day=="saturday" or delivery_day=="sunday")
+        if week_days:
+            if weekend and order_total<50:
                 delivery_fee=7.00
                 message="Weekend small-order surcharge applies"
-            elif delivery_day in weekend and order_total>=50:
+            elif weekend and order_total>=50:
                 delivery_fee=3.00
                 message="Standard weekend delivery fee applies"
-            elif delivery_day not in weekend and order_total<50:
+            elif not weekend and order_total<50:
                 delivery_fee=5.00
                 message="Standard weekday delivery fee applies"
             else:
@@ -26,6 +27,6 @@ if order_total.replace(".",'',1).isdigit():
     else:
         print("Sorry invalid amount. Enter amount between between $0 and $1000 only.")
 else:
-    print(f"You have enter Invalid amount: {order_total}\n"
+    print(f"You have enter invalid amount: {order_total}\n"
           f"Enter valid amount between $0 and $1000")
     
